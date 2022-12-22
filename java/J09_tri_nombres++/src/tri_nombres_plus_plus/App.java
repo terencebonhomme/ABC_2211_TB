54cefa49144dde772661116c2ad50d7d619aeec6
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2022-12-22 16:28:35 +0100

    Complete tri nombres plus plus

--- java/J09_tri_nombres++/src/tri_nombres_plus_plus/App.java
+++ java/J09_tri_nombres++/src/tri_nombres_plus_plus/App.java
@@ -23,20 +23,27 @@ public class App {
 		c = sc.nextDouble();
 		
 		if(a < b) {
-			if(b < c) {
-				System.out.println(a + "; " + b + "; " + c);
+			if(a < c) {
+				if(c < b) {
+					System.out.println(a + "; " + c + "; " + b);
+				} else {
+					System.out.println(a + "; " + b + "; " + c);
+				}
 			} else {
-				System.out.println(a + "; " + c + "; " + b);
+				System.out.println(c + "; " + a + "; " + b);
 			}
-		}
-		else {
+		} else {
 			if(a < c) {
 				System.out.println(b + "; " + a + "; " + c);
 			} else {
-				System.out.println(b + "; " + c + "; " + a);
-			}
+				if(c < b) {
+					System.out.println(c + "; " + b + "; " + a);
+				} else {
+					System.out.println(b + "; "+ c + "; " + a);
+				}
+			}			
 		}
-	
+						
 		sc.close();
 				
 		// FIN PROGRAMME
