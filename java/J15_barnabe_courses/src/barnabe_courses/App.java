063a18f89b18885c7fe44ecee685047a9a3f511a
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-02 15:41:33 +0100

    add exercise 'Barnabe fait ses courses'

---
+++ java/J15_barnabe_courses/src/barnabe_courses/App.java
@@ -0,0 +1,38 @@
+package barnabe_courses;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		// VARIABLES
+		
+		int s;
+		int magasin;
+		
+		magasin = 0;
+		
+		Scanner sc = new Scanner(System.in);				
+		
+		// DEBUT PROGRAMME
+		
+		System.out.println("Saisir une somme");
+		s = sc.nextInt();
+		
+		while(s > 0) {
+			
+			if(s == 1) {
+			} else {
+				s = s - (s/2 + 1);
+			}
+			
+			magasin++;
+		}
+		
+		System.out.println("Barnabe a depense la somme dans " + magasin + " magasin(s)");
+		
+		// FIN PROGRAMME
+	}
+
+}
