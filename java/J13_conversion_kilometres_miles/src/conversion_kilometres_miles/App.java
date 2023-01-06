c643e86b1f9b0ce81a1270f8bc7a04544e6645b8
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-06 11:56:38 +0100

    fix exercise conversion kilometres miles

--- java/J13_conversion_kilometres_miles/src/conversion_kilometres_miles/App.java
+++ java/J13_conversion_kilometres_miles/src/conversion_kilometres_miles/App.java
@@ -5,39 +5,39 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		
-		// VARIABLES
-		
-		double distance_km = 0;
-		double distance_miles;
-		String reponse = "";
-		
-		Scanner sc1 = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-		
-		// DEBUT PROGRAMME					
-		
-		while(!reponse.equals("q")) {	
-			System.out.println("Saisir une distance entre 0.01 et 1000000");
-			distance_km = sc2.nextDouble();
-			
-			if(distance_km >= 0.01 && distance_km <= 1000000) {				
-				System.out.println(distance_km / 1.609);
-			} else {
-				System.out.println("La valeur doit être comprise entre 0.01 et 1000000");
-			}	
-			
-			System.out.println("q pour quitter, autre pour continue");
-			reponse = sc1.nextLine();
-		}	
 
-		System.out.println("quitter");
+		// VARIABLES
+
+		Scanner sc = new Scanner(System.in);
+
+		double distance;
+		String reponse;
+		boolean quit = false;
+
+		// DEBUT PROGRAMME
+
+		while (!quit) {
+			System.out.println("Saisir une distance entre 0.01 et 1000000; q pour quitter");
+			reponse = sc.nextLine();
+
+			if (reponse.equals("q")) {
+				quit = true;
+			} else {
+				
+				distance = Double.parseDouble(reponse);
+				
+				if (distance >= 0.01 && distance <= 1000000) {
+					System.out.println(distance / 1.609);
+				} else {
+					System.out.println("La valeur doit être comprise entre 0.01 et 1000000");
+				}
+			}
+		}
 		
-		sc1.close();
-		sc2.close();
+		sc.close();
 		
 		// FIN PROGRAMME
-		
+
 	}
 
 }
