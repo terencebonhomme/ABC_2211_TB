2e5bcf900ffc04a8a26e41a2dfdfa239ef53a37d
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-11 11:29:47 +0100

    add exercise 1.2 tests

--- java/1.2_aire_volume_sphere/src/aire_volume_sphere/App.java
+++ java/1.2_aire_volume_sphere/src/aire_volume_sphere/App.java
@@ -13,6 +13,13 @@ public class App {
 		double pi = Math.PI;
 		Scanner sc = new Scanner(System.in);
 		
+		// TESTS
+		
+		System.out.println("test 1 : rayon = 3");
+		System.out.println("test 2 : rayon = 5");
+		System.out.println("test 3 : rayon = 12");
+		System.out.println("");
+		
 		// DEBUT PROGRAMME
 		
 		System.out.println("Veuillez écrire la valeur du rayon R");
@@ -27,6 +34,25 @@ public class App {
 		sc.close();
 		
 		// FIN PROGRAMME
+		
+		// DEBUT TESTS
+		
+		System.out.println("");
+		
+		if(rayon == 3) {
+			System.out.println("test 1 : aire est " + (aire >= 113.097 && aire <= 113.098) + " et volume est " + (volume >= 113.097 && volume <= 113.098));
+		}
+		
+		if(rayon == 5) {
+			System.out.println("test 2 : aire est " + (aire >= 314.159 && aire <= 314.160) + " et volume est " + (volume >= 523.598  && volume <= 523.599));
+		}
+
+		if(rayon == 12) {
+			System.out.println("test 3 : aire est " + (aire >= 1809.557 && aire <= 1809.558) + " et volume est " + (volume >= 7238.229  && volume <= 7238.230));
+		}
+
+		
+		// FIN TESTS
 	}
 
 }
