2fab6831781282bfb30671b2913305cf0cb91cbf
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-12 08:24:45 +0100

    add exercise 1.3 tests

--- java/1.3_aire_secteur_angulaire/src/aire_secteur_angulaire/App.java
+++ java/1.3_aire_secteur_angulaire/src/aire_secteur_angulaire/App.java
@@ -12,6 +12,13 @@ public class App {
 		double aire;		
 		Scanner sc = new Scanner(System.in);
 		
+		// JEU DE TESTS
+		
+		System.out.println("test 1 : rayon " + 5 + " et angle " + 60);
+		System.out.println("test 2 : rayon " + 7 + " et angle " + 30);
+		System.out.println("test 3 : rayon " + 10 + " et angle " + 180);
+		System.out.println("");
+		
 		// DEBUT PROGRAMME
 		
 		System.out.println("Veuillez écrire le rayon cercle et un angle");
@@ -24,6 +31,21 @@ public class App {
 		sc.close();
 		
 		// FIN PROGRAMME
+		
+		// DEBUT TESTS
+		
+		System.out.println("");		
+		if(rayon == 5 && angle == 60) {
+			System.out.println("test 1 : " + (aire >= 13.089 && aire <= 13.090));
+		}		
+		if(rayon == 7 && angle == 30) {
+			System.out.println("test 2 : " + (aire >= 12.828 && aire <= 12.829));
+		}		
+		if(rayon == 10 && angle == 180) {
+			System.out.println("test 3 : " + (aire >= 157.079 && aire <= 157.080));
+		}
+		
+		// FIN TESTS
 	}
 
 }
