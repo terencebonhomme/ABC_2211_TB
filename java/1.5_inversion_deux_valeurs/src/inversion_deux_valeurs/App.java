b7862394f09929c116653d7c414106f5b42ac355
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-12 08:48:20 +0100

    add exercise 1.5 tests

--- java/1.5_inversion_deux_valeurs/src/inversion_deux_valeurs/App.java
+++ java/1.5_inversion_deux_valeurs/src/inversion_deux_valeurs/App.java
@@ -13,6 +13,13 @@ public class App {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		// JEU DE TESTS
+		
+		System.out.println("test 1 : a = 5 et b = 7");
+		System.out.println("test 2 : a = 5 et b = 5");
+		System.out.println("test 3 : a = 10 et b = 2");
+		System.out.println("");
+		
 		// DEBUT PROGRAMME
 					
 		System.out.println("Ecrire A");
@@ -30,6 +37,22 @@ public class App {
 		sc.close();
 		
 		// FIN PROGRAMME
+		
+		// DEBUT TESTS
+		
+		System.out.println("");
+		if(a == 7 && b == 5) {
+			System.out.println("test 1 : " + (a == 7 && b == 5));	
+		}
+		if(a == 5 && b == 5) {
+			System.out.println("test 2 : " + (a == 5 && b == 5));	
+		}
+		if(a == 2 && b == 10) {
+			System.out.println("test 3 : " + (a == 2 && b == 10));	
+		}
+		
+		
+		// FIN TESTS
 	}
 
 }
