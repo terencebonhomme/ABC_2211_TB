ad66588fcb1a801901d0324db817250eb86af028
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-12 09:15:13 +0100

    add exercise 2.3 tests

--- java/2.3_tri_nombres++/src/tri_nombres_plus_plus/App.java
+++ java/2.3_tri_nombres++/src/tri_nombres_plus_plus/App.java
@@ -13,6 +13,30 @@ public class App {
 		
 		Scanner sc = new Scanner(System.in);		
 		
+		// JEU DE TESTS
+		
+		System.out.println("test 1 : a = 1, b = 2 et c = 3");
+		System.out.println("test 2 : a = 1, b = 3 et c = 2");
+		System.out.println("test 3 : a = 2, b = 1 et c = 3");
+		System.out.println("test 4 : a = 2, b = 3 et c = 1");
+		System.out.println("test 5 : a = 3, b = 1 et c = 2");
+		System.out.println("test 6 : a = 3, b = 2 et c = 1");
+		System.out.println("");
+		System.out.println("test 7 : a = 0,1, b = 0,2 et c = 0,3");
+		System.out.println("test 8 : a = 0,1, b = 0,3 et c = 0,2");
+		System.out.println("test 9 : a = 0,2, b = 0,1 et c = 0,3");
+		System.out.println("test 10 : a = 0,2, b = 0,3 et c = 0,1");
+		System.out.println("test 11 : a = 0,3, b = 0,1 et c = 0,2");
+		System.out.println("test 12 : a = 0,3, b = 0,2 et c = 0,1");
+		System.out.println("");
+		System.out.println("test 13 : a = 0,1, b = 0,2 et c = -0,3");
+		System.out.println("test 14 : a = 0,1, b = -0,3 et c = 0,2");
+		System.out.println("test 15 : a = -0,2, b = 0,1 et c = 0,3");
+		System.out.println("test 16 : a = 0,2, b = 0,3 et c = -0,1");
+		System.out.println("test 17 : a = 0,3, b = -0,1 et c = 0,2");
+		System.out.println("test 18 : a = -0,3, b = 0,2 et c = 0,1");
+		System.out.println("");
+		
 		// DEBUT PROGRAMME
 		
 		System.out.println("Saisir A");
