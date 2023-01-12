c0b80e064bb614055f44a3bff43204c439e310a1
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-12 08:53:42 +0100

    add exercise 2.1 tests

--- java/2.1_comparaison_nombres/src/comparaison_nombres/App.java
+++ java/2.1_comparaison_nombres/src/comparaison_nombres/App.java
@@ -10,6 +10,15 @@ public class App {
 		int age;
 		Scanner sc = new Scanner(System.in);
 		
+		// JEU DE TESTS
+		
+		System.out.println("test 1 : age = -1");
+		System.out.println("test 2 : age = 0");
+		System.out.println("test 3 : age = 10");
+		System.out.println("test 4 : age = 18");
+		System.out.println("test 5 : age = 30");
+		System.out.println("");
+			
 		// DEBUT PROGRAMME
 		
 		System.out.println("Saisir votre âge");
