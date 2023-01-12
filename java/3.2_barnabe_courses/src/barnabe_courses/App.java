c274b8273edb9c7b4bd1fb377f887b29b3d00737
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-12 12:48:33 +0100

    fix and test for exercise 3.2

--- java/3.2_barnabe_courses/src/barnabe_courses/App.java
+++ java/3.2_barnabe_courses/src/barnabe_courses/App.java
@@ -10,18 +10,38 @@ public class App {
 		
 		int s;
 		int magasin;
+		int init_s;
 		
 		magasin = 0;
 		
-		Scanner sc = new Scanner(System.in);				
+		Scanner sc = new Scanner(System.in);		
+		
+		// JEU DE TESTS
+		
+		System.out.println("test 1 : somme = 0");
+		System.out.println("test 2 : somme = 1");
+		System.out.println("test 3 : somme = 2");
+		System.out.println("test 4 : somme = 3");
+		System.out.println("test 5 : somme = 4");
+		System.out.println("test 6 : somme = 100");
+		System.out.println("");
 		
 		// DEBUT PROGRAMME
 		
 		System.out.println("Saisir une somme");
 		s = sc.nextInt();
+		init_s = s;
 		
 		while(s > 0) {
-			s = s - (s/2 + 1);	
+			if(s > 2) {
+				//System.out.println(s - (s/2 + 1) == s/2 - 1);
+				System.out.println(s + " - " + (s/2 + 1) + " = " + (s - (s/2 + 1)) + " car " + (s/2 + 1) + " < " + s + " donc " + (magasin + 1) + " magasin(s)");  
+				s = s - (s/2 + 1);				
+			}else {
+				System.out.println(s + " - " + s + " = " + 0 + " car " + (s/2 + 1) + " = " + s + " donc " + (magasin + 1) + " magasin(s)");  
+				s = 0;
+			}						
+				
 			magasin++;
 		}
 		
