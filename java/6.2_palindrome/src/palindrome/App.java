f794d9bdb53b8bd4ffb7ca0581504f36c9150207
BONHOMME BRENNER Terence <TBonhommebrenner@arfp.asso.fr>
2023-01-17 08:28:25 +0100

    add exercise 6.2

---
+++ java/6.2_palindrome/src/palindrome/App.java
@@ -0,0 +1,59 @@
+package palindrome;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// VARIABLES
+
+		String texte;
+		boolean estPalindrome;
+		int moitie;
+
+		estPalindrome = true;
+
+		Scanner sc = new Scanner(System.in);
+
+		// DEBUT PROGRAMME
+
+		System.out.print("Saisir un texte : ");
+		texte = sc.nextLine();
+
+		if (texte.length() == 0) {
+			System.out.println("LA CHAINE EST VIDE");
+		} else if (texte.length() == 1) {
+			System.out.println("CHAINE D'AU MOINS 2 CARACTERES");
+		} else {
+			// les espaces ne sont pas pris en compte dans l'un des exemple
+			texte = texte.replaceAll(" ", "");
+			
+			// stopper à la moitié pour ne pas vérifier 2 fois
+			if (texte.length() % 2 == 0) {
+				moitie = texte.length() / 2 - 1;
+			} else {
+				moitie = (texte.length() - 1) / 2;
+			}
+
+			// le texte est un palindrome jusqu'à preuve du contraire
+			for (int i = 0; i <= moitie && estPalindrome; i++) {
+				System.out.println(i + " -> " + texte.charAt(i) + " et " + texte.charAt(texte.length() - 1 - i));
+				if (texte.charAt(i) != texte.charAt(texte.length() - 1 - i)) {
+					estPalindrome = false;
+				}
+			}
+
+			if (estPalindrome) {
+				System.out.println("CE TEXTE EST UN PALINDROME");
+			} else {
+				System.out.println("CE TEXTE N'EST PAS UN PALINDROME");
+			}
+		}
+		
+		sc.close();
+
+		// FIN PROGRAMME
+	}
+
+}
